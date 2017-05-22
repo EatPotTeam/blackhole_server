539f5176a8c552ef349dcfb21de8232e4eff072a
oldface <1047801437@qq.com>
2017-05-23 00:32:12 +0800

    add schedule and fix bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
 
 
 app.use(bodyParser.json());
@@ -10,32 +11,44 @@ var messages = [];
 
 
 var Message = function (message, time) {
-	this.message = message;
-	this.time = time;
+    this.message = message;
+    this.time = time;
 }
 
+function scheduleCronstyle() {
+    schedule.scheduleJob('30 * * * * *', function(){
+        var newTime = Date.now()-60*1000;
+        var count = 0;
+        for (var i = 0; i < messages.length; i++) {
+            if (messages[i].time < newTime) count++;
+        }
+        messages.splice(0,count);
+        console.log(messages.length);
+    }); 
+}
+scheduleCronstyle();
 
 app.get('/login/:id', function (req, res) {
-	ids[req.params.id] = Date.now();
-	res.send("login success");
+    ids[req.params.id] = Date.now();
+    res.send("login success");
 });
 
 
 app.post('/submit', function (req, res) {
-	messages.push(new Message(req.body, Date.now()));
-	res.send("message accepted");
+    messages.push(new Message(req.body, Date.now()));
+    res.send("message accepted");
 });
 
 
 app.get('/pull/:id', function (req, res) {
-	data = [];
-	var latestTime = (Date.now() - 60*1000 > ids[req.params.id]) ? Date.now() - 60*1000 : ids[req.params.id];
-	for (i = messages.length-1; i >= 0 && messages[i].time >= latestTime; i--) data.push(messages[i]);
-	//res.body = JSON.stringify(data);
-	res.send(JSON.stringify(data));
+    data = [];
+    var latestTime = (Date.now() - 60*1000 > ids[req.params.id]) ? Date.now() - 60*1000 : ids[req.params.id];
+    for (i = messages.length-1; i >= 0 && messages[i].time >= latestTime; i--) data.push(messages[i]);
+    ids[req.params.id]=Date.now();
+    res.send(JSON.stringify(data));
 });
 
 
 app.listen(8080, function() {
-	console.log('listen at 8080');
+    console.log('listen at 8080');
 });
