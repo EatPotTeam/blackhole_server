b4f0608170b3c38330451f3de72fae9debe8332a
leo <leo.lizijie@gmail.com>
2017-06-22 18:02:54 +0800

    fix a bug and add a status code when response

--- app/controller/getLogin.controller.js
+++ app/controller/getLogin.controller.js
@@ -1,17 +1,18 @@
-var user = require('../model/user');
+user = require('../model/user');
 
 module.exports = function(req, res) {
-	var respond = {};
-	if (!user.exist(req.params.id)) {
+	respond = {};
+	if (user.exist(req.params.id)) {
 		respond.result = "login success";
+        res.send(JSON.stringify(respond));
 	} else {
 		respond.result = "login fail";
+        res.send(401, JSON.stringify(respond));
 	}
-	res.send(JSON.stringify(respond));
     // if (ids[req.params.id] !== undefined) {
     //     respond.result = "login success";
     //     ids[req.params.id] = Date.now();
     // }
     // else respond.result = "login fail";
     
-}
+};
